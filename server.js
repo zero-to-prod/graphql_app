f1bffbc58da3f430802bd4858ccb03874017b2e1
zero-to-prod <61474950+zero-to-prod@users.noreply.github.com>
2020-08-14 16:59:09 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,35 @@
 const { ApolloServer, gql } = require("apollo-server");
 
+const todos = [
+  { task: "Task 1", completed: false },
+  { task: "Task 2", completed: true },
+];
+
 // The GraphQL schema
 const typeDefs = gql`
+  type Todo {
+    task: String
+    completed: Boolean
+  }
   type Query {
-    "A simple type for getting started!"
-    hello: String
+    getTodos: [Todo]
+  }
+  type Mutation {
+    addTodo(task: String, completed: Boolean): Todo
   }
 `;
 
 // A map of functions which return data for the schema.
 const resolvers = {
   Query: {
-    hello: () => "world",
+    getTodos: () => todos,
+  },
+  Mutation: {
+    addTodo: (_, { task, completed }) => {
+      const todo = { task: task, completed: completed };
+      todos.push(todo);
+      return todo;
+    },
   },
 };
 
